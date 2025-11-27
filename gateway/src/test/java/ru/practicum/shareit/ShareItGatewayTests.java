b5dfe02ab26bae1c6aa6ad416eef2c4642f7469d
Nohum <savva.gubarev228@mail.ru>
2025-11-27 17:10:30 +0300

    1

--- gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTests.java
+++ gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTests.java
@@ -2,9 +2,11 @@ package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-public class ShareItGatewayTests {
+@ActiveProfiles("test")
+class ShareItGatewayTests {
 
     @Test
     void contextLoads() {
